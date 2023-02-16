02d07162229d22cbcc3515dba230413809dfc192
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-16 19:34:27 +0300

    Функции. вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ Seminar_1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28 4 -> 10 8 -> 36
+
+int GetSum(int A)
+{
+    int sum = 0;
+    if (A > 1)
+    {
+        for (int i = 1; i <= A; i++) // i = i + 1 // i += 1
+        {
+            sum += i; // sum = sum + i
+        }
+    }
+
+    return sum;
+}
+
+Console.Write("Введите А: ");
+int n = int.Parse(Console.ReadLine()!);
+int sum = GetSum(n);
+Console.WriteLine($"{sum}");
