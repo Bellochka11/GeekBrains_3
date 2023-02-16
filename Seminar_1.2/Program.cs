70cb14f3121b469ceb68a712a16216f20fb24e27
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-16 19:50:01 +0300

    Функция. вход число и выдаёт количество цифр в числе

---
+++ Seminar_1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3 78 -> 2 89126 -> 5
+
+
+void main()
+{
+    Console.Write("Введите число: ");
+    int num = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(result(num));
+}
+
+int result(int number)
+{
+    if (number < 0) number *= -1; // если пользователь вводит отрицат число то умножаем на -1 чтобы получить положит число
+    int count = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+ main();
+
+
+
