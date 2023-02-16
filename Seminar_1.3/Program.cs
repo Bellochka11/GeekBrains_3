6b0e9cd6fd68b049d8d7c03676c15be31edbaed3
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-16 20:09:11 +0300

    Функция.вход число N и выдаёт произведение чисел от 1 до N

---
+++ Seminar_1.3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24  5 -> 120
+
+
+int Mult(int arg)
+{
+    int x = 1;
+    for (int i = 1; i <= arg; i++) // i = i + 1 // i += 1
+    {
+        x *= i; // sum = sum + i
+    }
+
+    return x;
+}
+
+Console.Write("Введите N: ");
+int a = int.Parse(Console.ReadLine()!);
+int total = Mult(a);
+Console.WriteLine($"{total}");
+
+
+
