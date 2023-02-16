708a179b099238f2f7b8e604d6c53a5f7d7bb31d
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-16 20:27:30 +0300

    Функция массив из 8 элементов, заполненный нулями и единицами

---
+++ Seminar_1.4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+void printArray(int[] array)
+{
+    string ourArray = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        ourArray += array[i];
+        if (i != array.Length - 1) ourArray += ", ";
+    }
+    Console.WriteLine(ourArray);
+}
+
+
+void main()
+{
+    int[] intArray = new int[8];
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        int randInd = new Random().Next(0, 2);
+        intArray[i] = randInd;
+    }
+    printArray(intArray);
+}
+main();
+
